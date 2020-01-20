137a5190253082ff0dd0005eb4a9f5b0573e0376
teneons <teneons@gmail.com>
2020-01-20 16:33:09 +0200

    Add state - done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,15 +9,13 @@ import ToDoListItem from "../ToDoListItem/ToDoListItem";
 
 export default class App extends React.Component {
 
-
-
   render() {
       const itemData = [
-          {id: 0, itemText: 'Item 0', important: false},
-          {id: 1, itemText: 'Item 1', important: true},
-          {id: 2, itemText: 'Item 2', important: true},
-          {id: 3, itemText: 'Item 3', important: true},
-          {id: 4, itemText: 'Item 4', important: false},
+          {id: 0, itemText: 'Item 0', important: false, done: false},
+          {id: 1, itemText: 'Item 1', important: true, done: false},
+          {id: 2, itemText: 'Item 2', important: true, done: false},
+          {id: 3, itemText: 'Item 3', important: true, done: false},
+          {id: 4, itemText: 'Item 4', important: false, done: false},
       ];
 
 
