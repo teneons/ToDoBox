473ce37da324eb2cf0caaf9a987ab82d17a9bd8c
teneons <teneons@gmail.com>
2020-01-21 01:59:56 +0200

    Make - filter btns

--- src/components/ToDoFilter/ToDoFilter.js
+++ src/components/ToDoFilter/ToDoFilter.js
@@ -4,7 +4,11 @@ export default class ToDoFilter extends React.Component {
     render() {
         return(
             <div>
-                <button>F</button>
+                <div className="btn-group mb-2" role="group" aria-label="Filter items">
+                    <button type="button" className="btn btn-lg btn-outline-warning border border-warning">Active</button>
+                    <button type="button" className="btn btn-lg btn-outline-secondary border border-warning">Done</button>
+                    <button type="button" className="btn btn-lg btn-outline-dark text-uppercase border border-warning">Deleted</button>
+                </div>
             </div>
         )
     }
