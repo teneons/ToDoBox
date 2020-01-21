639d38b81b4cd97ef446ac400394be7ec71d6b22
teneons <teneons@gmail.com>
2020-01-21 13:17:10 +0200

    Fix search

--- src/components/ToDoSearch/ToDoSearch.js
+++ src/components/ToDoSearch/ToDoSearch.js
@@ -7,7 +7,7 @@ export default class ToDoSearch extends React.Component {
         const txtPlaceholder = 'Search by list';
 
         return(
-            <div className="col-3">
+            <div className='col-3 mb-2'>
                 <input className='form-control col' type='text' placeholder={txtPlaceholder} />
             </div>
         );
