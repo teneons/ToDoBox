951ab6a3172983d90ca28ad472fd0da7e2873e1f
teneons <teneons@gmail.com>
2020-01-21 13:26:53 +0200

    Fix styles for items

--- src/components/ToDoListItem/ToDoListItem.js
+++ src/components/ToDoListItem/ToDoListItem.js
@@ -6,18 +6,18 @@ export default class ToDoListItem extends React.Component {
     render() {
         const {itemText, important } = this.props;
 
-
         const style = {
-            color: important? 'red' : 'black'
+            color: important? 'red' : 'black',
+            fontWeight: important? '800' : '400'
         };
 
         return(
             <div>
                 <span style={style}>{itemText}{important}</span>
                 <div className='float-right'>
-                    <button className='btn btn-sm btn-danger mr-1'><i className="fa fa-star"></i></button>
-                    <button className='btn btn-sm btn-secondary mr-1'><i className="fa fa-check-square-o"></i></button>
-                    <button className='btn btn-sm btn-dark mr-1'><i className="fa fa-trash-o"></i></button>
+                    <button className='btn btn-sm btn-danger mr-1'><i className='fa fa-star'></i></button>
+                    <button className='btn btn-sm btn-secondary mr-1'><i className='fa fa-check-square-o'></i></button>
+                    <button className='btn btn-sm btn-dark mr-1'><i className='fa fa-trash-o'></i></button>
                 </div>
             </div>
         )
