c6b7970078f1e6261893a9acd66e51178534aefd
teneons <teneons@gmail.com>
2020-01-21 13:22:56 +0200

    Fix filters

--- src/components/ToDoFilter/ToDoFilter.js
+++ src/components/ToDoFilter/ToDoFilter.js
@@ -4,10 +4,10 @@ export default class ToDoFilter extends React.Component {
     render() {
         return(
             <div>
-                <div className="btn-group mb-2" role="group" aria-label="Filter items">
-                    <button type="button" className="btn btn-lg btn-outline-warning border border-warning">Active</button>
-                    <button type="button" className="btn btn-lg btn-outline-secondary border border-warning">Done</button>
-                    <button type="button" className="btn btn-lg btn-outline-dark text-uppercase border border-warning">Deleted</button>
+                <div className='btn-group mb-2' role='group' aria-label='Filter items'>
+                    <button type='button' className='btn btn-lg btn-outline-warning border border-warning text-uppercase'>Active</button>
+                    <button type='button' className='btn btn-lg btn-outline-secondary border border-warning text-uppercase'>Done</button>
+                    <button type='button' className='btn btn-lg btn-outline-dark border border-warning text-uppercase'>Deleted</button>
                 </div>
             </div>
         )
