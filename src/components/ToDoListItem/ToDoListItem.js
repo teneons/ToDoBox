67d91b82c4831fe67721e883ba22b3ed82e188c4
teneons <teneons@gmail.com>
2020-01-22 13:39:17 +0200

    Add atribute - done

--- src/components/ToDoListItem/ToDoListItem.js
+++ src/components/ToDoListItem/ToDoListItem.js
@@ -1,22 +1,46 @@
 import React from 'react';
+import './ToDoListItem.css';
 
 export default class ToDoListItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            important: false,
+            done: false
+        };
+
+        this.importantClick = () => {
+            this.setState(({important})=>{
+                return {important: !important}
+            })
+        };
+        this.doneClick = () => {
+            this.setState(({done})=> {
+                return { done: !done}
+            })
+        };
+    };
 
 
     render() {
-        const {itemText, important } = this.props;
+        const {itemText} = this.props;
 
-        const style = {
-            color: important? '#dc3545' : 'black',
-            fontWeight: important? '800' : '400'
-        };
+        let  nameClss = 'ToDoListItem';
+        if(this.state.important){
+            nameClss += ' important';
+        }
+        if(this.state.done){
+            nameClss += ' done';
+        }
 
         return(
             <div>
-                <span style={style}>{itemText}{important}</span>
+                <span className={nameClss}>
+                    <span className='ToDoListItemText'>{itemText}{this.state.important}{this.state.done}</span>
+                </span>
                 <div className='float-right'>
-                    <button className='btn btn-sm btn-danger mr-1'><i className='fa fa-star'></i></button>
-                    <button className='btn btn-sm btn-secondary mr-1'><i className='fa fa-check-square-o'></i></button>
+                    <button onClick={this.importantClick} className='btn btn-sm btn-danger mr-1'><i className='fa fa-star'></i></button>
+                    <button onClick={this.doneClick} className='btn btn-sm btn-secondary mr-1'><i className='fa fa-check-square-o'></i></button>
                     <button className='btn btn-sm btn-dark mr-1'><i className='fa fa-trash-o'></i></button>
                 </div>
             </div>
